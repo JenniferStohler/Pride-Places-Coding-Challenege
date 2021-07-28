46b8d584471f3649a416abb70f6a2fdf753a6515
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-07-28 15:17:33 -0600

    using Axios to make an API call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 
 function App() {
+
+  //useState 
+  const [post, setPost] = useState("");
+
+  //Use Axios to call to the API and fetch posts, then console.log the response to see if you've successfully retrie ved the posts from the API.
+
+  const getPost = () => {
+    Axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      console.log(response);
+    })
+  }
+
+  //Make a card for the posts to show up on.
+
   return (
-    <div>
-      <h1 className="text-3xl">Hello!</h1>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <button onClick={getPost}>Get Post</button>
     </div>
   );
 }
