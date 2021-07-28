fa4e4cd4e92aa532418e57f4f22eafde7cd36d1e
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-07-28 17:54:09 -0600

    changed getPosts and getUsers to just useEffect

--- src/App.js
+++ src/App.js
@@ -1,42 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import PostsCard from "./components/PostsCard";
 
 
 function App() {
 
   //useState 
   const [post, setPost] = useState([]);
-  const [user, setUser] = useState([])
-    ;
-  //Call to the API and fetch posts, then console.log the response to see if you've successfully retrie ved the posts from the API.
-
-  const getPost = () => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((data) => {
-        setPost(data[1].title)
-        console.log(data)
-    });
-  }
-
-  const getUser = () => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then(data => {
-        setUser(data[0].company.name)
-        console.log(data)
-      });
-  }
-
-  //Make a card for the posts to show up on.
+  const [user, setUser] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+    
+  //Call to the API and fetch posts, then console.log the response to see if you've successfully retrieved the posts from the API.
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+  }, []);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+  }, []);
 
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <button onClick={getPost}>Get Post</button>
-      {post}
-      <button onClick={getUser}>Get User</button>
-      {user}
+
     </div>
-  );
+  )
 }
+
 
 export default App;
--- src/components/PostsCard.js
+++ src/components/PostsCard.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const PostsCard = ({ post }) => {
   <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      {post}
-    </div>
+ 
+</div>
 }
 
 
