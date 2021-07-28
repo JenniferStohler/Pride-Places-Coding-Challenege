2d0d9d7575fcec8026354a7791b292398606f0aa
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-07-28 16:59:41 -0600

    Posts and Users data retrieved from API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import Axios from 'axios';
+
 
 function App() {
 
   //useState 
-  const [post, setPost] = useState("");
-
-  //Use Axios to call to the API and fetch posts, then console.log the response to see if you've successfully retrie ved the posts from the API.
+  const [post, setPost] = useState([]);
+  const [user, setUser] = useState([])
+    ;
+  //Call to the API and fetch posts, then console.log the response to see if you've successfully retrie ved the posts from the API.
 
   const getPost = () => {
-    Axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
-      console.log(response);
-    })
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        setPost(data[1].title)
+        console.log(data)
+    });
+  }
+
+  const getUser = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(data => {
+        setUser(data[0].company.name)
+        console.log(data)
+      });
   }
 
   //Make a card for the posts to show up on.
@@ -19,6 +32,9 @@ function App() {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <button onClick={getPost}>Get Post</button>
+      {post}
+      <button onClick={getUser}>Get User</button>
+      {user}
     </div>
   );
 }
--- src/components/PostsCard.js
+++ src/components/PostsCard.js
@@ -1,1 +1,16 @@
 import React from 'react';
+
+const PostsCard = ({ post }) => {
+  <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      {post}
+    </div>
+}
+
+
+
+
+
+
+
+
+export default PostsCard;
