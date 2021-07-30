3a9ce8fd4df9512f7b1b0d60aaaf2072124355cc
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-07-30 00:05:26 -0600

    more styling

--- src/components/PostsCard.js
+++ src/components/PostsCard.js
@@ -19,17 +19,17 @@ export default function PostsCard(props) {
                 <div className="post" key={index}>
                   <div className="max-w-md rounded overflow-hidden shadow-lg px-5 py-5 bg-white">  
                     <h3 className="post_title">{post.title}</h3>
-                    <p className="post_body">{post.body}</p>
-                    <button onClick={() => setShowModal(true)} className="btn px-2 py-2 rounded bg-green-400 text-white">
-                      Click Here to Open Modal
-                    </button>
-                    <Modal isOpen={showModal}
-                      onRequestClose={() => setShowModal(false)}
-                      ariaHideApp={false}
-                      contentLabel="Selected Option"
-                    >
-                      <button className="btn px-2 py-2 rounded bg-red-400 text-white" onClick={() => setShowModal(false)}>Close Modal</button>
-                      </Modal>
+                      <p className="post_body">{post.body}</p>
+                        <button onClick={() => setShowModal(true)} className="btn px-2 py-2 rounded bg-green-400 text-white">
+                          Click Here to Open Modal
+                           </button>
+                            <Modal isOpen={showModal}
+                              onRequestClose={() => setShowModal(false)}
+                              ariaHideApp={false}
+                              contentLabel="Selected Option"
+                            >
+                              <button className="btn px-2 py-2 rounded bg-red-400 text-white" onClick={() => setShowModal(false)}>Close Modal</button>
+                            </Modal>
                   
                   </div>
                 </div>
